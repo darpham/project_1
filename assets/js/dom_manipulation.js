0589a1fbbda45c22b0238c6bb4dba0e59a2c89f7
Darren Pham <phamdarren1@gmail.com>
2019-04-30 02:42:34 -0700

    working on ability to update restaurant suggestions on index.html with dom js

--- assets/js/dom_manipulation.js
+++ assets/js/dom_manipulation.js
@@ -10,6 +10,8 @@ $(document).ready(function(){
 // --------------------------------------------------------------------------------
         // Add functionality to display recommended restaurants
 // --------------------------------------------------------------------------------
+        dom_manipulation.addSuggestions(result.businesses);
+        
     };
 
     api_obj.yelpToCuisine (
@@ -33,14 +35,15 @@ $("#search").on("click", function() {
 // --------------------------------------------------------------------------------
         // Add functionality to display recipes
 // --------------------------------------------------------------------------------
-
+        
     };
     
 // --------------------------------------------------------------------------------
         // Change this function below to pass the right params
 // --------------------------------------------------------------------------------
 
-    api_obj.yelpToCuisine("burma love"
+    api_obj.yelpToCuisine(
+        "burma love"
         , "San Francisco"
         , [] // ["kale", "lettuce", "tomato"] // - ingredients
         , [] // ["kiwi", "pineapple"] // - exclusion
@@ -51,3 +54,23 @@ $("#search").on("click", function() {
     console.log("clicked button"); 
 });
 
+dom_manipulation = {
+    // Nested forEach functions to create divs with appropiate styling for each restaurant
+    addSuggestions : function(businesses) {
+        // splice to remove unnecessary amount of restaurants (6-19)
+        businesses.splice(6,14);
+        console.log(businesses)
+
+        businesses.forEach(function(business) {
+            var suggestion = $('<div>');
+            suggestion.addClass('col-md-4 suggested');
+            suggestion.append('<img class="imgportf" src="' +business.image_url+ '" />');
+            suggestion.append('<h4>' +business.name+ '</h4>');
+
+            $('#suggestions').append(suggestion);
+        });
+
+        $('#loading').addClass('hidden');
+        $('#suggestions').removeClass('hidden');
+    },
+};
