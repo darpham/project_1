1581cec83d0412d36971bd4b27f0282cb83c5da8
takeohiraki <takeohiraki@gmail.com>
2019-05-01 19:45:14 -0700

    dedupe default restaurants

--- assets/js/dom_manipulation.js
+++ assets/js/dom_manipulation.js
@@ -2,14 +2,31 @@ dom_manipulation = {
     // Nested forEach functions to create divs with appropiate styling for each restaurant
     addSuggestions : function(businesses) {
         // splice to remove unnecessary amount of restaurants (6-19)
-        businesses.splice(6,14);
-        console.log(businesses)
+        // businesses.splice(6,14);
+        console.log("businesses before dedupe: ");
+        console.log(businesses);
 
-        businesses.forEach(function(business) {
+        var businessArr = [];
+        var businessObj = [];
+        console.log(businesses[0].name)
+        console.log(businesses[0])
+        businesses.some(function(business) {
+            if(!businessArr.includes(business.name)) {
+                businessObj.push(business);
+                businessArr.push(business.name)
+                console.log("test");
+                console.log(businessObj);
+            }
+            return businessObj.length === 6;
+        });
+        console.log("businesses after dedupe: ");
+        console.log(businessObj);
+
+        businessObj.forEach(function(business) {
             var suggestion = $('<div>');
             suggestion.addClass('col-md-4 suggested');
-            suggestion.append('<img class="imgportf suggested-rest" restaurant=' +business.name+ ' src="' +business.image_url+ '" />');
-            suggestion.append('<h5 class="suggested-rest" restaurant=' +business.name+ ' >' +business.name+ '</h5>');
+            suggestion.append('<img class="imgportf suggested-rest" restaurant="' +business.name+ '" src="' +business.image_url+ '" />');
+            suggestion.append('<h5 class="suggested-rest" restaurant="' +business.name+ '" >' +business.name+ '</h5>');
             $('#suggestions').append(suggestion);
         });
 
