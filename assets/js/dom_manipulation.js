7d9f55a3bec93a56e2060fbd6d23e4444d28f358
Darren Pham <phamdarren1@gmail.com>
2019-05-02 06:00:05 -0700

    added function to create recipes on result.html and fixed some inputs

--- assets/js/dom_manipulation.js
+++ assets/js/dom_manipulation.js
@@ -1,26 +1,16 @@
 dom_manipulation = {
     // Nested forEach functions to create divs with appropiate styling for each restaurant
     addSuggestions : function(businesses) {
-        // splice to remove unnecessary amount of restaurants (6-19)
-        // businesses.splice(6,14);
-        console.log("businesses before dedupe: ");
-        console.log(businesses);
 
         var businessArr = [];
         var businessObj = [];
-        console.log(businesses[0].name)
-        console.log(businesses[0])
         businesses.some(function(business) {
             if(!businessArr.includes(business.name)) {
                 businessObj.push(business);
                 businessArr.push(business.name)
-                console.log("test");
-                console.log(businessObj);
             }
             return businessObj.length === 6;
         });
-        console.log("businesses after dedupe: ");
-        console.log(businessObj);
 
         businessObj.forEach(function(business) {
             var suggestion = $('<div>');
@@ -34,4 +24,24 @@ dom_manipulation = {
         $('#loading').addClass('hidden');
         $('#suggestions').removeClass('hidden');
     },
+
+    // function to create and append recipes onto the result.html
+    addRecipes : function(recipes) {
+
+        // splice to remove unnecessary amount of restaurants (6-19)
+        recipes.splice(6,4);
+        console.log(recipes)
+
+        recipes.forEach(function(recipe) {
+            var suggestion = $('<div>');
+            suggestion.addClass('col-md-4 results');
+            suggestion.append('<img class="imgportf recipe" src="' +recipe.recipe.image+ '" />');
+            suggestion.append('<h5 class="recipe" >' +recipe.recipe.label+ '</h5>');
+            $('#suggested-recipes').append(suggestion);
+        });
+
+        // hides loading image and unhides suggestion divs once all suggestions are loadded above
+        $('#loading').addClass('hidden');
+        $('#suggested-recipes').removeClass('hidden');
+    }
 }; 
